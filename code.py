c1a1cae0796eaed3fb2a43a04ce95a7a07a3d87c
RUNTRO <73249957+RUNTRO@users.noreply.github.com>
2023-11-02 17:37:09 -0400

    Update code.py
    To create the code, I had to first define what turning forward, backward, left and right meant and which wheel it would turn. I then called each function and for how long it should run for to make it traverse the path.

--- code.py
+++ code.py
@@ -1,9 +1,15 @@
+import time
+import board
+import digitalio
+import pwmio
+
+
 from adafruit_motor import motor
 
-left_motor_forward = board.GP15
-left_motor_backward = board.GP14
-right_motor_forward = board.GP12
-right_motor_backward = board.GP13
+left_motor_forward = board.GP12
+left_motor_backward = board.GP13
+right_motor_forward = board.GP14
+right_motor_backward = board.GP15
 
 pwm_La = pwmio.PWMOut(left_motor_forward, frequency=10000)
 pwm_Lb = pwmio.PWMOut(left_motor_backward, frequency=10000)
@@ -17,50 +23,58 @@ Left_Motor_speed = 0
 Right_Motor = motor.DCMotor(pwm_Ra, pwm_Rb)
 Right_Motor_speed = 0
 
-    def forward():
+def forward():
     Right_Motor_speed = .5
     Right_Motor.throttle = Right_Motor_speed
-    time.sleep(0)
     Left_Motor_speed = .5
     Left_Motor.throttle = Left_Motor_speed
 
-    def backward():
+def backward():
     Right_Motor_speed = -0.5
     Right_Motor.throttle = Right_Motor_speed
-    time.sleep(0)
     Left_Motor_speed = -0.5
     Left_Motor.throttle = Left_Motor_speed
 
-    def left():
-    Right_Motor_speed = .5
-    Right_Motor.throttle = Right_Motor_speed
-
-
-
-    def right():
-    Left_Motor_speed = 0.5
+def left():
+    Left_Motor_speed = 0
     Left_Motor.throttle = Left_Motor_speed
-    Right_Motor_speed = 0
+    Right_Motor_speed = 0.5
     Right_Motor.throttle = Right_Motor_speed
 
-
-    def stop():
+def right():
     Right_Motor_speed =0
     Right_Motor.throttle = Right_Motor_speed
-    time.sleep(0)
+    Left_Motor_speed = 0.5
+    Left_Motor.throttle = Left_Motor_speed
+
+def stop():
+    Right_Motor_speed =0
+    Right_Motor.throttle = Right_Motor_speed
     Left_Motor_speed = 0
     Left_Motor.throttle = Left_Motor_speed
 
-    while True:
-    #forward
-    Right_Motor_speed = .5
-    Right_Motor.throttle = Right_Motor_speed
-    time.sleep(0)
-    Left_Motor_speed = .5
-    Left_Motor.throttle = Left_Motor_speed
+while True:
 
-    #right
-    Left_Motor_speed = 0.5
-    Left_Motor.throttle = Left_Motor_speed
-    Right_Motor_speed = 0
-    Right_Motor.throttle = Right_Motor_speed
+    forward()
+    time.sleep(5)
+    left()
+    time.sleep(1)
+    forward()
+    time.sleep(4)
+    left()
+    time.sleep(1)
+    forward()
+    time.sleep(1)
+    left()
+    time.sleep(0.5)
+    forward()
+    time.sleep(2)
+    right()
+    time.sleep(2.5)
+    forward()
+    time.sleep(0.5)
+    forward()
+    time.sleep(5)
+    stop()
+
+
